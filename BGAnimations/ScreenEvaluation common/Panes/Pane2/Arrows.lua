b7f7d1b99f6a3b9f6168c957e4a8c80179062fb7
hayoreo <nimjero@gmail.com>
2023-06-20 17:02:34 -0700

    Clean an code.
    This is how i should have implemented hiding the early judgements when the rescores are disabled oops

--- BGAnimations/ScreenEvaluation common/Panes/Pane2/Arrows.lua
+++ BGAnimations/ScreenEvaluation common/Panes/Pane2/Arrows.lua
@@ -80,18 +80,19 @@ for i, column in ipairs( cols ) do
 					if j == #rows then miss_bmt = self end
 				end
 			}
-			if judgment == "W4" or judgment == "W5" then
-				af[#af+1] = LoadFont("Common Normal")..{
-					Text=SL[pn].Stages.Stats[SL.Global.Stages.PlayedThisGame + 1].column_judgments[i]["Early"][judgment],
-					InitCommand=function(self)
-						-- don't show these early counts if we have them disabled.
-						self:visible(ThemePrefs.Get("RescoreEarlyHits"))
-						self:xy(_x - 1, j*row_height - 6):zoom(0.65):halign(1)
-					end,
-					OnCommand=function(self)
-						self:x( self:GetX() - miss_bmt:GetWidth()/2 )
-					end
-				}
+			-- don't show these early counts if we have them disabled.
+			if ThemePrefs.Get("RescoreEarlyHits") then
+				if judgment == "W4" or judgment == "W5" then
+					af[#af+1] = LoadFont("Common Normal")..{
+						Text=SL[pn].Stages.Stats[SL.Global.Stages.PlayedThisGame + 1].column_judgments[i]["Early"][judgment],
+						InitCommand=function(self)
+							self:xy(_x - 1, j*row_height - 6):zoom(0.65):halign(1)
+						end,
+						OnCommand=function(self)
+							self:x( self:GetX() - miss_bmt:GetWidth()/2 )
+						end
+					}
+				end
 			end
 		end
 	end
