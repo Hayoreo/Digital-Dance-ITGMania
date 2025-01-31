31e292729b9a116d379afc5abaf521133fee6903
hayoreo <nimjero@gmail.com>
2025-01-30 16:41:23 -0800

    Clean up subtractive score code.
    This changes nothing functionally, just removing unused code/cleaning it up.

--- BGAnimations/ScreenGameplay underlay/PerPlayer/Notefield/SubtractiveScoring.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/Notefield/SubtractiveScoring.lua
@@ -9,23 +9,15 @@ local NoteCount = chart:GetRadarValues(player):GetValue('RadarCategory_TapsAndHo
 local HoldCount = chart:GetRadarValues(player):GetValue('RadarCategory_Holds')
 local RollCount = chart:GetRadarValues(player):GetValue('RadarCategory_Rolls')
 local MineCount = chart:GetRadarValues(player):GetValue('RadarCategory_Mines')
-local Score
-local PossibleScore
-local CurrentPossibleScore
-local ActualScore
+local Score, PossibleScore, CurrentPossibleScore, ActualScore
 local HasFailed = false
 local IsNumber = true
 local FAPlusCount = 0
 local HeldCount = 0
-local ex_actual
-local ex_possible
-local ex_score
 local MinesHit = 0
 local HoldsRollsDropped = 0
--- -----------------------------------------------------------------------
-
-local undesirable_judgment = mods.ShowEXScore and "W1" or "W2"
 local undesirable_judgment_count = 0
+-- -----------------------------------------------------------------------
 
 local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(player)
 
@@ -55,8 +47,7 @@ local GetCurrentExScore = function(player, ex_counts, PotentialNotes, PotentialH
 	-- Stamina community does often play with no-mines on, but because EX scoring is more timing centric where mines
 	-- generally have a negative weight, it's a better experience to make sure the EX score reflects that.
 	if po:NoMines() then
-		local totalMines = StepsOrTrail:GetRadarValues(player):GetValue( "RadarCategory_Mines" ) - MinesHit
-		total_points = total_points + totalMines * SL.ExWeights["HitMine"];
+		total_points = total_points + MineCount * SL.ExWeights["HitMine"];
 	end
 	
 	local keys = { "W0", "W1", "W2", "W3", "W4", "W5", "Miss", "Held", "LetGo", "HitMine" }
@@ -191,15 +182,15 @@ bmt.ExCountsChangedMessageCommand=function(self, params)
 			self:settext( ("-%.2f%%"):format(percent) )
 		end
 		if not HasFailed then
-			ex_possible = GetPossibleExScore(params.ExCounts)
+			PossibleScore = GetPossibleExScore(params.ExCounts)
 			if undesirable_judgment_count > 10 then
 				IsNumber = false
 			end
 			if not IsNumber then
-				ex_score = 100 - ex_possible
+				Score = 100 - PossibleScore
 				-- handle floating point equality.
-				if ex_score >= 0.0001 then
-					self:settext( ("-%.2f%%"):format(ex_score) )
+				if Score >= 0.0001 then
+					self:settext( ("-%.2f%%"):format(Score) )
 				end
 			elseif undesirable_judgment_count > 0 and undesirable_judgment_count < 11 then
 				self:settext("-" .. undesirable_judgment_count)
